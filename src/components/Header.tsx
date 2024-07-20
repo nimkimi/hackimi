1501136f0c909abd922247d8d7444f707470682f
nima-hakimi <hakimi.nima1@gmail.com>
2024-07-20 11:45:36 +0200

    exprimenting with styling and dark mode using tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,47 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import '@/app/globals.css';
 
 export default function Header() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex w-full justify-between items-center bg-transparent py-4 px-6">
-      <Link href="/" className="logo">
+    <nav className="flex w-full justify-between dark:bg-bg-dark items-center bg-bg-light py-4 px-6">
+      <Link
+        href="/"
+        className="text-lg font-bold text-text-light dark:text-text-dark"
+      >
         N.HAKIMI
       </Link>
       <div className="flex space-x-4">
         <Link
-          href="#"
+          href="/about"
           className={`${
-            pathname === '/about' ? 'bg-gray-900 text-white' : ''
-          } px-3 py-2 rounded-md text-sm font-medium`}
+            pathname === '/about'
+              ? 'bg-gray-900 text-white'
+              : 'text-text-light dark:text-text-dark'
+          } px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300`}
         >
           About
         </Link>
         <Link
-          href="#"
+          href="/projects"
           className={`${
-            pathname === '' ? 'bg-gray-900 text-white' : ''
-          } px-3 py-2 rounded-md text-sm font-medium`}
+            pathname === '/projects'
+              ? 'bg-gray-900 text-white'
+              : 'text-text-light dark:text-text-dark'
+          } px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300`}
         >
-          Project
+          Projects
         </Link>
         <Link
           href="/contact"
           className={`${
-            pathname === '/contact' ? 'bg-gray-900 text-white' : ''
-          } px-3 py-2 rounded-md text-sm font-medium`}
+            pathname === '/contact'
+              ? 'bg-dark text-dark dark:bg-light dark:text-light'
+              : 'text-light dark:text-dark'
+          } px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300`}
         >
           Contact
         </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  mode: 'jit', // Enable JIT mode
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'media', // Automatically switch to dark mode based on user's system preferences
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // Define your color palette centrally
+      colors: {
+        'bg-light': 'var(--color-bg-light)', // Light theme background color
+        'bg-dark': 'var(--color-bg-dark)', // Dark theme background color
+        'text-light': 'var(--color-text-light)', // Light theme text color
+        'text-dark': 'var(--color-text-dark)', // Dark theme text color
+      },
+      // Extend other styles if needed
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        light: '#f7fafc', // Fallback or light theme background color
+        dark: '#2d3748', // Dark theme background color
+      }),
+      textColor: (theme) => ({
+        ...theme('colors'),
+        light: '#111827', // Fallback or light theme text color
+        dark: '#f9fafb', // Dark theme text color
+      }),
+      container: {
+        center: true,
+        padding: '1rem',
       },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
+
 export default config;
